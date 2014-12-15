bf85e5d3decba586f664397dc1ce1f85cb22255c
nicholasnadel <nicholasnadel@gmail.com>
2014-12-14 23:56:56 -0800

    replicated code

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  port:              587,
+  port:              587, 
   address:           'smtp.mailgun.org',
   user_name:         ENV['MAILGUN_SMTP_LOGIN'],
   password:          ENV['MAILGUN_SMTP_PASSWORD'],
-  domain:            'appurlhere.com',
+  domain:            'app32213585.mailgun.org',
   authentication:    :plain,
   content_type:      'text/html'
 }
@@ -12,7 +12,7 @@ ActionMailer::Base.delivery_method = :smtp
 # Makes debugging *way* easier.
 ActionMailer::Base.raise_delivery_errors = true
 
-# This interceptor just makes sure that local mail
+# This interceptor just makes sure that local mail 
 # only emails you.
 # http://edgeguides.rubyonrails.org/action_mailer_basics.html#intercepting-emails
 class DevelopmentMailInterceptor
